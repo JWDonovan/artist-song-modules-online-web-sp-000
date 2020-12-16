be514906ea75d11ffd899e6237722dbc24de99c0
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-16 16:11:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,35 @@
 require 'pry'
 
 class Artist
+  #dup
   attr_accessor :name
   attr_reader :songs
 
+  #dup
   @@artists = []
 
   def initialize
+    #dup
     @@artists << self
     @songs = []
   end
 
+  # dup
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
 
+  # dup
   def self.all
     @@artists
   end
 
+  # dup
   def self.reset_all
     self.all.clear
   end
 
+  # dup
   def self.count
     self.all.count
   end
@@ -36,6 +43,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
+  # dup
   def to_param
     name.downcase.gsub(' ', '-')
   end
